5b803f10eb01dc3bb55e52b9862a1065edd60551
Danielle Dempsey <ddempsey@perennia.ca>
2021-02-01 16:17:31 -0400

    County reports for 2021-01-15 Open Data submission
    Reports published on website 2021-01-28
    
    Data release scheduled for 2021-02-05

--- Generate_County_Reports.R
+++ Generate_County_Reports.R
@@ -1,4 +1,6 @@
 
+library(lubridate)
+
 # This script generates county reports for all counties specified in the <<county>> variable
 
 # SECTION 1: Specify counties for which to generate report
@@ -12,45 +14,113 @@
 
 report <- "Y:/Coastal Monitoring Program/Open Data/County Reports/County_Report.Rmd"
 
-county <- c("Halifax", "Yarmouth")
+# county <-  c("Annapolis", "Digby", "Guysborough",
+#              "Halifax", "Inverness", "Lunenburg",
+#              "Pictou", "Richmond", "Shelburne",
+#              "Yarmouth")
 
+county <-  c("Halifax", "Richmond", "Shelburne")
 
 # SECTION 2: Document History ---------------------------------------------
 
+d1 <- as_date("2020-Oct-14")
+
+doc_date <- Sys.Date()
+doc_amendment <- "New data release"
+
 ## Halifax
 hist.Halifax <- data.frame(
-  `Version Number` = c(1, 2),
-  Date = c("2020-Oct-14", "2020-Nov-27"),
-  Amendments = c("New document", "SOP Example")
+  `Version Number` = c("V1", "V2"),
+  Date = c(d1, doc_date),
+  Amendments = c("New document", "New data release & additional QAQC of previous release"),
+  check.names = FALSE
 ) 
 
 ## Lunenburg
 hist.Lunenburg <- data.frame(
-  `Version Number` = c(1),
-  Date = c("2020-Oct-14"),
-  Amendments = c("New document")
+  `Version Number` = c("V1", "V2"),
+  Date = c(d1, doc_date),
+  Amendments = c("New document", doc_amendment),
+  check.names = FALSE
 ) 
 
 ## Guysborough
 hist.Guysborough <- data.frame(
-  `Version Number` = c(1),
-  Date = c("2020-Oct-14"),
-  Amendments = c("New document")
+  `Version Number` = c("V1", "V2"),
+  Date = c(d1, doc_date),
+  Amendments = c("New document", "New data release & additional QAQC of previous release"),
+  check.names = FALSE
+) 
+
+
+## Annapolis
+hist.Annapolis <- data.frame(
+  `Version Number` = c("V1"),
+  Date = c(doc_date),
+  Amendments = c("New document"),
+  check.names = FALSE
+) 
+
+## Digby
+hist.Digby <- data.frame(
+  `Version Number` = c("V1"),
+  Date = c(doc_date),
+  Amendments = c("New document"),
+  check.names = FALSE
+) 
+
+## Inverness
+hist.Inverness <- data.frame(
+  `Version Number` = c("V1"),
+  Date = c(doc_date),
+  Amendments = c("New document"),
+  check.names = FALSE
+) 
+
+## Pictou
+hist.Pictou <- data.frame(
+  `Version Number` = c("V1"),
+  Date = c(doc_date),
+  Amendments = c("New document"),
+  check.names = FALSE
+) 
+
+## Richmond
+hist.Richmond <- data.frame(
+  `Version Number` = c("V1"),
+  Date = c(doc_date),
+  Amendments = c("New document"),
+  check.names = FALSE
+) 
+
+## Shelburne
+hist.Shelburne <- data.frame(
+  `Version Number` = c("V1"),
+  Date = c(doc_date),
+  Amendments = c("New document"),
+  check.names = FALSE
 ) 
 
 ## Yarmouth
 hist.Yarmouth <- data.frame(
-  `Version Number` = c(1),
-  Date = c("2020-Nov-27"),
-  Amendments = c("New document")
+  `Version Number` = c("V1"),
+  Date = c(doc_date),
+  Amendments = c("New document"),
+  check.names = FALSE
 ) 
 
 
 # Document history for each county
 DOC.HIST <- list(
+  Annapolis = hist.Annapolis,
+  Digby = hist.Digby,
+  Guysborough = hist.Guysborough,
   Halifax = hist.Halifax,
+  Inverness = hist.Inverness,
   Lunenburg = hist.Lunenburg,
-  Guysborugh = hist.Guysborough,
+  Pictou = hist.Pictou,
+  Richmond = hist.Richmond,
+  Shelburne = hist.Shelburne,
   Yarmouth = hist.Yarmouth
 )
 
